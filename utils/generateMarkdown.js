b63f7114e4bfa701826ab6630144f559effb90e3
Tooboi <japica123@gmail.com>
2022-12-04 18:27:56 -0500

    markdown export set up, beginning inquirer questions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,45 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Table of Contents
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+
+  ## Description
+
+  ${data.description}
+  
+  ## Installation
+
+  ${data.installation}
+  
+  ## Usage
+
+  ${data.usage}
+  
+  ## License
+
+  ${data.license}
+  
+  ## Contributing
+
+  ${data.contributing}
+  
+  ## Tests
+
+  ${data.tests}
+  
+  ## Questions
+
+  ${data.questions}
 `;
 }
 
